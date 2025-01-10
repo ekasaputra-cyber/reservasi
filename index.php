5b9623a54ca27055ee7bbad7778ec84f92040ac9
ekasptr-cyber <ekasptr581@gmail.com>
2025-01-11 00:29:12 +0700

    .

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // Redirect pengguna ke halaman utama (misalnya, daftar pelanggan)
 header("Location: pages/reservasi.php");
-// header("Location: index.html");
+// header("Location: index.html"); 
 exit();
